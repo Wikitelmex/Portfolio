12fba0348eb1a7fab68583c09ba728966c7e63ce
Alex Castillo <wikitelmex@gmail.com>
2021-08-31 10:50:35 -0600

    first commit

--- script.js
+++ script.js
@@ -6,5 +6,29 @@ function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
 }
 
+
+//testing functions modal
+var modal = document.getElementById("myModal");
+
+var body = document.getElementsByTagName("body");
+
+function openModal() {
+  
+  modal.style.display = "block";
+  modalcontent.style.display ="block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 module.exports = openNav();
 module.exports = closeNav();
+module.exports = openModal();
+module.exports = closeModal();
