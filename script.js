39202ab1d9013a42d8715c6d654478d30f6f5102
Alex Castillo <wikitelmex@gmail.com>
2021-09-01 09:18:35 -0600

    linter errors fix
    - fixed linter errors

--- script.js
+++ script.js
@@ -6,36 +6,33 @@ function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
 }
 
-
-//testing functions modal
-var modal=document.getElementById("myModal");
-var container=document.getElementById("myContainer");
-var body=document.getElementsByTagName("body")
+// testing functions modal
+const modal = document.getElementById('myModal');
+const container = document.getElementById('myContainer');
+const body = document.getElementsByTagName('body');
 
 function openModal() {
-  modal.className="Modal is-visuallyHidden";
-  container.className="MainContainer is-blurred";
-  modal.className="Modal";
-  container.parentElement.className="ModalOpen";
+  modal.className = 'Modal is-visuallyHidden';
+  container.className = 'MainContainer is-blurred';
+  modal.className = 'Modal';
+  container.parentElement.className = 'ModalOpen';
 }
 
 function closeModal() {
-  modal.className="Modal is-hidden is-visuallyHidden";
-  body.className="";
-  container.className="MainContainer";
-  container.parentElement.className="";
+  modal.className = 'Modal is-hidden is-visuallyHidden';
+  body.className = '';
+  container.className = 'MainContainer';
+  container.parentElement.className = '';
 }
 
-window.onclick = function(e){
-  e.target==modal&&(
-      modal.className="Modal is-hidden",
-      body.className="",
-      container.className="MainContainer",
-      container.parentElement.className=""
-  )
+window.onclick = function wclick(event) {
+  if (event.target === modal) {
+    modal.className = 'Modal is-hidden';
+    body.className = '';
+    container.className = 'MainContainer';
+    container.parentElement.className = '';
+  }
 };
-
-
 
 module.exports = openNav();
 module.exports = closeNav();
